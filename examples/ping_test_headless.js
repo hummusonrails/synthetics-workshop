fb8237819eabc9a94cedab190c6d0668a16115e1
Ben Greenberg <bengreenberg@gmail.com>
2022-03-15 10:28:47 +0200

    add workflow and script for headless ping test

--- examples/ping_test_headless.js
+++ examples/ping_test_headless.js
@@ -3,7 +3,10 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function pingTest() {
-  let driver = await new Builder().forBrowser('chrome').build();
+  let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().addArguments('--headless'))
+    .build();
 
   await driver.get('https://express-xbdl.onrender.com').then(() => {
     console.log('The site is alive!')
